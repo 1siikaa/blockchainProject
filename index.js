0010e5bd3cf6657db066bd08eb0dd4d79ea4ce37
1siikaa <srishtidwivedi672@gmail.com>
2022-12-09 12:13:43 +0530

    done

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express= require('express')
 const app= express()
 const route= require('./src/route/route.js')
 
-app.use(express.json())
-
 
 app.use('/', route)
 
 app.listen(3000, function(){
-    console.log("Express app is running on PORT"+ 3000)
+    console.log("Express app is running on PORT" + 3000)
 })
